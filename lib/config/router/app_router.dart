20f01b61ed05d8d737d258e26216d7159017fd7d
NicolasOrdoV <65665096+NicolasOrdoV@users.noreply.github.com>
2024-01-15 08:27:38 -0500

    go route mejorado

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,48 +1,20 @@
 import 'package:cinemapedia/presentation/screens/movies/home_screen.dart';
 import 'package:cinemapedia/presentation/screens/movies/movie_screen.dart';
-import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
-  ShellRoute(
-    builder: (context, state, child) {
-      return HomeScreen(childView: child);
-    },
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) {
-          return const HomeView();
-        },
-        routes: [
-          GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
-          builder: (context, state) {
-            final movieId = state.pathParameters['id'] ?? 'no-id';
-            return MovieScreen(
-              movieId: movieId,
-            );
-          },
-        ),
-        ]
-      ),
-      GoRoute(
-        path: '/favorites',
-        builder: (context, state) {
-          return const FavoritesView();
-        },
-      ),
-    ],
-  ),
-  // GoRoute(
-  //     path: '/',
-  //     name: HomeScreen.name,
-  //     builder: (context, state) => const HomeScreen(
-  //           childView: HomeView(),
-  //         ),
-  //     routes: [
-  //       GoRoute(
+final appRouter = GoRouter(initialLocation: '/home/0', routes: [
+  // ShellRoute(
+  //   builder: (context, state, child) {
+  //     return HomeScreen(childView: child);
+  //   },
+  //   routes: [
+  //     GoRoute(
+  //       path: '/',
+  //       builder: (context, state) {
+  //         return const HomeView();
+  //       },
+  //       routes: [
+  //         GoRoute(
   //         path: 'movie/:id',
   //         name: MovieScreen.name,
   //         builder: (context, state) {
@@ -52,5 +24,39 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
   //           );
   //         },
   //       ),
-  //     ]),
+  //       ]
+  //     ),
+  //     GoRoute(
+  //       path: '/favorites',
+  //       builder: (context, state) {
+  //         return const FavoritesView();
+  //       },
+  //     ),
+  //   ],
+  // ),
+  GoRoute(
+      path: '/home/:page',
+      name: HomeScreen.name,
+      builder: (context, state) { 
+        final pageIndex = state.pathParameters['page'] ?? '0';
+        return HomeScreen(pageIndex: int.parse(pageIndex) ,);
+      },
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) {
+            final movieId = state.pathParameters['id'] ?? 'no-id';
+            return MovieScreen(
+              movieId: movieId,
+            );
+          },
+        ),
+      ]),
+  GoRoute(
+    path: '/',
+    redirect: ( _ , __) {
+      return '/home/0';
+    }
+  )
 ]);
--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -2,34 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
 
-  int getCurrentIndex(BuildContext context) {
+  final int currentIndex;
+  const CustomBottomNavigation({super.key, required this.currentIndex});
 
-    final String location = GoRouterState.of(context).location;
+  int getCurrentIndex( BuildContext context ) {
+    final String location = GoRouterState.of(context).matchedLocation;
 
     switch(location) {
       case '/':
         return 0;
+      
       case '/categories':
         return 1;
+
       case '/favorites':
         return 2;
+      
       default:
-        return 0;  
+        return 0;
     }
   }
 
   void onItemTapped(BuildContext context, int index){
     switch( index ) {
       case 0:
-        context.go('/');
+        context.go('/home/0');
         break;
       case 1:
-        context.go('/');
+        context.go('/home/1');
         break;
       case 2:
-        context.go('/favorites');
+        context.go('/home/2');
         break;
     }
   }
@@ -39,7 +43,7 @@ class CustomBottomNavigation extends StatelessWidget {
 
     return BottomNavigationBar(
       elevation: 0,
-      currentIndex: getCurrentIndex(context),
+      currentIndex: currentIndex,
       onTap: (index) {
         onItemTapped(context, index);
       },
